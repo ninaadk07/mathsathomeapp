3c2de866e41c7339023f5f934c23dc3ce8ba4f78
EgeGulce <zcabegu@ucl.ac.uk>
2024-01-23 17:17:04 +0000

    fixed restapi connection issue

--- my_app/restAPI/app.py
+++ my_app/restAPI/app.py
@@ -2,9 +2,6 @@ import pyodbc
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(debug=True)
     
 # Database connection function
 def get_db_connection():
@@ -42,3 +39,38 @@ def add_child():
     conn.commit()
     conn.close()
     return "Child added", 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    get_children()
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/database/children', methods=['GET'])
+def get_children():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Child;')
+    children = cursor.fetchall()
+    children_list = [{'ID': row[0], 'FamilyID': row[1], 'Name': row[2], 'DOB': row[3].strftime('%Y-%m-%d'), 'Sex': row[4], 'Country': row[5], 'LevelOfEducation': row[6]} for row in children]
+    conn.close()
+    return jsonify(children_list)
+
+@app.route('/database/child', methods=['POST'])
+def add_child():
+    new_child = request.json
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    query = '''
+    INSERT INTO Child (FamilyID, Name, DOB, Sex, Country, LevelOfEducation)
+    VALUES (?, ?, ?, ?, ?, ?);
+    '''
+    cursor.execute(query, (new_child['FamilyID'], new_child['Name'], new_child['DOB'], new_child['Sex'], new_child['Country'], new_child['LevelOfEducation']))
+    conn.commit()
+    conn.close()
+    return "Child added", 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
